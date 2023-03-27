9902a5f41eb6a909d720a2ae73c449c60135fd4d
Dasari001 <107811202+Dasari001@users.noreply.github.com>
2023-03-27 11:39:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 // complete the given function
 
 function palindrome(str){
-	flag = true;
-   for(var i=0; i<str.length/2; i++){
-	   if(str.charAt(i)!=str.charAt(str.length-1-i)){
-		   flag = false;
-	       break;
-	   }
-   }
-	if(flag==false){
-		return false;
-	}
-	else{
-		return true;
-	}
+	// flag = true;
+ //   for(var i=0; i<str.length/2; i++){
+	//    if(str.charAt(i)!=str.charAt(str.length-1-i)){
+	// 	   flag = false;
+	//        break;
+	//    }
+ //   }
+	// if(flag==false){
+	// 	return false;
+	// }
+	// else{
+	// 	return true;
+	// }
+
+
+
+	str = str.toLowerCase().replace(/[\W_]/g, '');
+  
+  // Reverse the string
+  let reversedStr = str.split('').reverse().join('');
+  
+  // Compare the original string with the reversed string
+  return str === reversedStr;
 }
 module.exports = palindrome

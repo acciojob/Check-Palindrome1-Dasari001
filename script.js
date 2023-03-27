0c1707e489179d3345df49d046d30e06a44665d3
Dasari001 <107811202+Dasari001@users.noreply.github.com>
2023-03-27 11:06:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // complete the given function
 
 function palindrome(str){
-
+	flag = true;
+   for(var i=0; i<str.length/2; i++){
+	   if(str.charAt(i)!=str.charAt(str.length-1-i))
+		   flag = false;
+   }
+	if(flag==false){
+		return false;
+	}
+	else{
+		return true;
+	}
 }
 module.exports = palindrome
